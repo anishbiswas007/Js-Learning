604e7f0d68b8ad04a0aae6d27aa7319eabfca0ad
anishbiswas007 <anish.biswas@webnware.in>
2025-05-18 10:22:38 +0000

    veriable

---
+++ 01_basic/veriables.js
@@ -0,0 +1,21 @@
+const myName = 123456
+let myMobileNum = "9660958763"
+var myCity = "jaipur"
+password = "123456"
+
+
+// myName = 2 // not allowed
+
+
+myMobileNum = "9024741007"
+myCity = "Raj"
+password = "22222"
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([myName, myMobileNum, myCity, password])
+
